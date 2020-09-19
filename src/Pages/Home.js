d747c56e3a5188c8fe198d5ce1a5b354b94ce1f6
FACTS  LIMITED <admin@factsafrica.com>
2020-09-19 17:28:12 +0300

    create home component

---
+++ src/Pages/Home.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { gapi } from 'gapi-script';
+import moment from 'moment';
+import Button from '@material-ui/core/Button';
+import AddIcon from '@material-ui/icons/Add';
+import LockIcon from '@material-ui/icons/Lock';
+import { Grid } from '@material-ui/core';
+import Header from '../Header';
+import Home from '../../Pages/Home/Home';
+import AddLesson from '../../Pages/AddEvent/AddEvent'
+import './Login.css';
+
+const Home = (props) => {
+
+}
+
+export default Home
